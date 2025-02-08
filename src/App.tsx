4e44753df9bef146ef3aad0811a63cd7751d45d2
huanlong <3025897003@qq.com>
2025-02-08 15:27:59 +0800

    添加必要的开发组件 antd 与tailwindcss等

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import {Button} from 'antd';
+
 function App() {
 
     return (
         <>
-            <h1>Vite + React</h1>
+            <h1 className = "p-7">
+                Vite + React</h1>
+            <Button type = "primary"> Button</Button>
         </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
+import './assets/global.scss'
+import './assets/tailwindcss.css'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
