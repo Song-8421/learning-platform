ebd8fa96736fbd83e6c613a9291c5fae78063671
huanlong <3025897003@qq.com>
2025-02-11 16:29:27 +0800

    lg 响应式

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,54 @@
 // import React from 'react'
 
+import {Config} from "@icon-park/react";
+import menus from '../config/menus.ts'
+import {Button, Drawer} from 'antd';
+import {useState} from 'react'
+
 function NavBar() {
+    const [open, setOpen] = useState(true);
+
+    const showDrawer = () => {
+        setOpen(true);
+    };
+
+    const onClose = () => {
+        setOpen(false);
+    };
     return (
-        <main className = 'bg-white'>
-            <section className = ''>
-                48
-            </section>
-        </main>
+        <>
+            <main className = ' bg-white flex justify-center'>
+                <section className = 'w-full flex justify-between py-3 container pl-4 size-fit  pr-[10px] '>
+                    <div className = 'flex items-center justify-center gap-2 '>
+                        <div className = "logo flex items-center text-rose-600 gap-2">
+                            <Config theme = "outline" size = '28'/>
+                            <div className = "name text-2xl uppercase font-bold ">
+                                xihaha
+                            </div>
+                        </div>
+                        <div className = 'lg:flex item-center gap-2 hidden'>
+                            {
+                                menus.map(menu => (
+                                    <a href = '#' key = {menu.title} className = "flex items-center ">
+                                        {menu.title}
+                                    </a>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div className = 'flex gap-2'>
+                        <Button type = {'primary'}>登录</Button>
+                        <Button type = {"default"}>注册</Button>
+                    </div>
+                </section>
+            </main>
+            <Drawer title = "Basic Drawer" onClose = {onClose} open = {open}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Drawer>
+        </>
+
     )
 }
 
---
+++ src/config/menus.ts
@@ -0,0 +1,12 @@
+export default [
+    {title: "系统课程", to: '/'},
+    {title: "实战项目", to: '/'},
+    {title: "碎片课程", to: '/'},
+    {title: "最近更新", to: '/'},
+    {title: "话题讨论", to: '/'},
+    {title: "签到打卡", to: '/'},
+    {title: "订阅优惠", to: '/'},
+    {title: "兑换码", to: '/'},
+    {title: "直播地址", to: '/'},
+    {title: "在线文档", to: '/'},
+]
